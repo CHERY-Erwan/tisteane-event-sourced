5b10db336e684e554889c1ad5f3c860ed676dc77
CHERY Erwan <chery.erwan@laposte.net>
2025-02-28 11:47:31 +0100

    Refactor CartIdentifiersData to use UuidInterface instead of Uuid

--- app/Domains/Cart/Data/CartIdentifiersData.php
+++ app/Domains/Cart/Data/CartIdentifiersData.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Domains\Cart\Data;
 
-use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 use Spatie\LaravelData\Data;
 
 final class CartIdentifiersData extends Data
 {
     public function __construct(
-        public readonly ?Uuid $customerUuid,
+        public readonly ?UuidInterface $customerUuid,
         public readonly string $sessionId,
     ) {}
 
