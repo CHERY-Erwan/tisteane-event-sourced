de319f6b59aea984824537bccdb63773919fc132
CHERY Erwan <chery.erwan@laposte.net>
2025-03-22 21:20:52 +0100

    Ajout des fichiers de langue pour les pages de produits en anglais et en français, incluant des traductions pour les détails du produit, la disponibilité et d'autres éléments du modal.

---
+++ resources/lang/en/pages/product.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'open_product' => 'Details',
+    'modal' => [
+        'in_stock' => 'In stock',
+        'out_of_stock' => 'Out of stock',
+    ],
+];
---
+++ resources/lang/fr/pages/product.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'open_product' => 'Détails',
+    'modal' => [
+        'in_stock' => 'En stock',
+        'out_of_stock' => 'Rupture',
+        'breadcrumbs' => [
+            'products' => 'Produits',
+        ],
+        'size' => 'Taille',
+        'color' => 'Couleur',
+        'add_to_cart' => 'Ajouter au panier',
+        'product_added_to_cart' => 'Produit ajouté au panier',
+    ],
+];
