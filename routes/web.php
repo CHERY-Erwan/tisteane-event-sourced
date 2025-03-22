2025261e7f909bd14dbc442b2b4b7e18d2d561ae
CHERY Erwan <chery.erwan@laposte.net>
2025-03-22 21:21:05 +0100

    Ajout d'une nouvelle route pour la page d'accueil, permettant d'afficher la vue 'homepage'. Mise à jour mineure du fichier de routes.

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/homepage', function () {
+    return view('homepage');
+})->name('homepage');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -21,4 +25,4 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
