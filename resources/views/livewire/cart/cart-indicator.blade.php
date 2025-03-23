cc4b176a4436f1d7a788c1af71222ff45710b155
CHERY Erwan <chery.erwan@laposte.net>
2025-03-23 23:01:28 +0100

    Ajout d'un composant Livewire pour l'indicateur de panier, affichant le nombre d'articles dans le panier et permettant la mise à jour dynamique lors des événements de panier. Amélioration de l'interaction utilisateur avec un affichage visuel du nombre d'articles.

---
+++ resources/views/livewire/cart/cart-indicator.blade.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\On;
+
+new class extends Component {
+
+    public int $cartItemCount = 0;
+
+    public function mount()
+    {
+        $this->cartItemCount = 3;
+    }
+
+    #[On("cart-updated")]
+    public function updateCartCount()
+    {
+        $this->cartItemCount = 5;
+    }
+};
+?>
+
+<div class="relative">
+    <div
+        x-on:click="$dispatch('show-cart')"
+        class="w-fit p-3 h-12 rounded-lg hover:cursor-pointer border border-zinc-600 bg-zinc-600/75 flex items-center justify-center"
+    >
+        <flux:icon name="shopping-cart" class="shrink-0 w-6 h-6 text-white"/>
+        @if ($cartItemCount > 0)
+            <div class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full h-5 w-5 flex items-center justify-center text-xs">
+                {{ $cartItemCount }}
+            </div>
+        @endif
+    </div>
+</div>
