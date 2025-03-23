ae5c5aa9d8b4d2c55ed584136f2a1a788868b0f1
CHERY Erwan <chery.erwan@laposte.net>
2025-03-23 22:39:35 +0100

    Ajout de nouvelles traductions pour les détails des produits, y compris des incitations telles que la garantie, la politique de retours et les informations sur la livraison, améliorant ainsi l'expérience utilisateur sur la page produit.

--- resources/lang/fr/pages/product.php
+++ resources/lang/fr/pages/product.php
@@ -12,5 +12,17 @@ return [
         'color' => 'Couleur',
         'add_to_cart' => 'Ajouter au panier',
         'product_added_to_cart' => 'Produit ajouté au panier',
+        'show_more' => 'Afficher plus',
+        'show_less' => 'Afficher moins',
+        'incentives' => [
+            'assembled' => 'Marque française',
+            'assembled_text' => 'Conçu et assemblé à Lyon.',
+            'warranty' => 'Garantie',
+            'warranty_text' => 'Inclus 1 an de garantie.',
+            'returns_policy' => 'Politique de retours',
+            'returns_text' => 'Vous avez 30 jours pour vous désister et nous renvoyer votre produit.',
+            'shipping' => 'Livraison',
+            'shipping_text' => 'Livraison gratuite pour toute commande supérieure à 50€.',
+        ],
     ],
 ];
