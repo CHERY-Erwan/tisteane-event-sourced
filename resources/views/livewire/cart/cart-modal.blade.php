3647f7484bc7e92418d9a29a3b7fdda3947e26b1
CHERY Erwan <chery.erwan@laposte.net>
2025-03-24 19:45:28 +0100

    Refactorisation des méthodes dans le composant du modal du panier et du produit, ajout de types de retour pour améliorer la clarté du code. Simplification du calcul du prix total et mise à jour de l'affichage des éléments pour une meilleure lisibilité.

--- resources/views/livewire/cart/cart-modal.blade.php
+++ resources/views/livewire/cart/cart-modal.blade.php
@@ -1,39 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 use Livewire\Attributes\On;
 use Livewire\Volt\Component;
-use Livewire\Attributes\Js;
 use App\Domains\Cart\Projections\Cart;
 use App\Domains\Cart\Actions\CheckoutCartAction;
 
 new class extends Component {
     public bool $isOpen = false;
-
     public ?Cart $cart = null;
-
     public int $totalCartPrice = 0;
 
     #[On("show-cart")]
-    public function showCart()
+    public function showCart(): void
     {
         $this->cart = request()->attributes->get('cart');
-
         $this->refreshCart();
-
         $this->isOpen = true;
     }
 
     #[On("refresh-cart")]
-    public function refreshCart()
+    public function refreshCart(): void
     {
         $this->cart?->refresh();
-
-        $this->totalCartPrice = $this->cart->items->reduce(function ($carry, $item) {
-            return $carry + ($item->item->price * $item->quantity);
-        }, 0);
+        $this->totalCartPrice = $this->cart?->items->sum(
+            fn ($item) => $item->item->price * $item->quantity
+        ) ?? 0;
     }
 
-    public function checkout(CheckoutCartAction $action)
+    public function checkout(CheckoutCartAction $action): void
     {
         $action();
 
@@ -52,13 +48,16 @@ new class extends Component {
         <flux:separator />
         @if ($cart)
             @foreach ($cart->items->sortByDesc('created_at') as $item)
-                <livewire:cart.cart-product :item="$item" wire:key="cart-product-{{ $item->item->uuid }}-{{ $item->quantity }}" />
+                <livewire:cart.cart-product
+                    :item="$item"
+                    wire:key="cart-product-{{ $item->item->uuid }}-{{ $item->quantity }}"
+                />
             @endforeach
         @endif
     </div>
 
     <div class="mt-auto">
-    <flux:separator />
+        <flux:separator />
         <div class="flex justify-between my-4">
             <div class="flex flex-col gap-1 w-2/3">
                 <flux:text>{{ __("pages/cart.modal.subtotal") }}</flux:text>
@@ -67,7 +66,11 @@ new class extends Component {
             <flux:text size="xl">{{ number_format($totalCartPrice / 100, 2, ',', ' ') }}€</flux:text>
         </div>
 
-        <flux:button icon="arrow-right" class="w-full h-10 mb-4 hover:cursor-pointer" wire:click="checkout">
+        <flux:button
+            icon="arrow-right"
+            class="w-full h-10 mb-4 hover:cursor-pointer"
+            wire:click="checkout"
+        >
             {{ __("pages/cart.modal.checkout") }}
         </flux:button>
 
--- resources/views/livewire/cart/cart-product.blade.php
+++ resources/views/livewire/cart/cart-product.blade.php
@@ -7,19 +7,13 @@ use App\Domains\Cart\Actions\UpdateItemQuantityAction;
 use App\Domains\Cart\Actions\RemoveItemFromCartAction;
 use App\Domains\Shared\Data\QuantityData;
 use Livewire\Volt\Component;
-use Livewire\Attributes\On;
 use Livewire\Attributes\Computed;
 
 new class extends Component
 {
     public CartItem $item;
 
-    public function mount(CartItem $item)
-    {
-        $this->item = $item;
-    }
-
-    public function updateItemQuantity(int $quantity, UpdateItemQuantityAction $action)
+    public function updateItemQuantity(int $quantity, UpdateItemQuantityAction $action): void
     {
         $action(
             itemUuid: $this->item->item->uuid,
@@ -30,7 +24,7 @@ new class extends Component
         $this->dispatch('refresh-cart');
     }
 
-    public function removeItemFromCart(RemoveItemFromCartAction $action)
+    public function removeItemFromCart(RemoveItemFromCartAction $action): void
     {
         $action(
             itemUuid: $this->item->item->uuid,
@@ -43,15 +37,16 @@ new class extends Component
     #[Computed]
     public function getTotalPrice(): string
     {
-        return number_format($this->item->item->price / 100 * $this->item->quantity, 2, ',', '.');
+        return number_format($this->item->item->price * $this->item->quantity / 100, 2, ',', '.');
     }
 }; ?>
 
 <div>
     <div class="flex gap-3 my-6">
         <div class="w-[92px] h-[92px] flex-shrink-0 border border-neutral-200 rounded-lg bg-white flex items-center justify-center overflow-hidden shadow-lg">
-            <img src="{{ $item?->item?->getMedia('color_attachment')->first()->getUrl('webp') }}"
-                alt="Thumbnail of {{ $item?->item?->product?->short_label }}"
+            <img
+                src="{{ $item->item->getMedia('color_attachment')->first()->getUrl('webp') }}"
+                alt="Thumbnail of {{ $item->item->product->short_label }}"
                 class="w-20 h-20 object-cover object-center"
                 loading="lazy"
                 decoding="async"
@@ -61,11 +56,11 @@ new class extends Component
         <div class="flex flex-col gap-2 w-full justify-between">
             <div class="flex justify-between">
                 <div>
-                    <flux:heading size="lg">{{ $item?->item?->product?->short_label }}</flux:heading>
+                    <flux:heading size="lg">{{ $item->item->product->short_label }}</flux:heading>
                     <flux:text>
-                        {{ $item?->item?->size?->label }}
+                        {{ $item->item->size->label }}
                         -
-                        {{ $item?->item?->color?->label }}
+                        {{ $item->item->color->label }}
                     </flux:text>
                 </div>
                 <div>
@@ -75,11 +70,25 @@ new class extends Component
 
             <div class="flex justify-between">
                 <flux:button class="w-1/3 h-10 !flex !flex-row !justify-around !items-center">
-                    <flux:icon name="minus" class="w-4 h-4 hover:cursor-pointer" wire:click="updateItemQuantity({{ $item->quantity - 1 }})" />
+                    <flux:icon
+                        name="minus"
+                        class="w-4 h-4 hover:cursor-pointer"
+                        wire:click="updateItemQuantity({{ $item->quantity - 1 }})"
+                    />
                     {{ $item->quantity }}
-                    <flux:icon name="plus" class="w-4 h-4 hover:cursor-pointer" wire:click="updateItemQuantity({{ $item->quantity + 1 }})" />
+                    <flux:icon
+                        name="plus"
+                        class="w-4 h-4 hover:cursor-pointer"
+                        wire:click="updateItemQuantity({{ $item->quantity + 1 }})"
+                    />
                 </flux:button>
-                <flux:button wire:click="removeItemFromCart" size="sm" variant="subtle" :loading="false" class="hover:underline hover:cursor-pointer">
+                <flux:button
+                    wire:click="removeItemFromCart"
+                    size="sm"
+                    variant="subtle"
+                    :loading="false"
+                    class="hover:underline hover:cursor-pointer"
+                >
                     {{ __("pages/cart.modal.delete") }}
                 </flux:button>
             </div>
